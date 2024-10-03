bc4b6563cc245db3942b69bd8dcc27b952cfdd92
Haaziq Jamal <121727878+Haaziq386@users.noreply.github.com>
2024-10-04 02:55:36 +0530

    Update snmp_trap-client.py

--- snmp_trap/snmp_trap-client.py
+++ snmp_trap/snmp_trap-client.py
@@ -11,15 +11,25 @@ trap_engine = SnmpEngine()
 
 # Trap target configuration (Trap Receiver)
 async def send_snmp_trap():
+    trap_oid = ObjectIdentity("1.3.6.1.4.1.8072.2.3.0.1")  # Example OID for testing
+    value = OctetString("This is a test value")  # Example value to send
+
+    # Define additional OID and value to send
+    additional_oid = ObjectIdentity("1.3.6.1.4.1.8072.2.3.0.2")  # Additional OID
+    additional_value = OctetString("Additional Test Value")  # Additional value
+
     error_indication, error_status, error_index, var_binds = await sendNotification(
         trap_engine,
         CommunityData(community_string),
         await UdpTransportTarget.create((server_ip, 162)),  # Async function call
         ContextData(),
         "trap",
-        NotificationType(
-            ObjectIdentity("1.3.6.1.4.1.8072.2.3.0.1")  # OID for testing purposes
-        ),
+        # NotificationType(
+        #     ObjectIdentity("1.3.6.1.4.1.8072.2.3.0.1")  # OID for testing purposes
+        # ),
+        NotificationType(trap_oid),  # Sending trap OID
+        (trap_oid, value),  # Main OID with value
+        (additional_oid, additional_value),
     )
 
     # Check for errors
